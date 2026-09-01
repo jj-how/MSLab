17d8a0fc14dcd208224b971ac2c8848b277e105e
JJ-How <jjh798800@gmail.com>
2026-09-01 11:26:44 +0900

    Update initialLabData.ts

--- src/data/initialLabData.ts
+++ src/data/initialLabData.ts
@@ -353,7 +353,8 @@ We warmly welcome aspiring researchers who are passionate about discovering hidd
       role: 'integrated',
       roleTitleKo: '석박통합과정',
       roleTitleEn: 'Integrated Ph.D.',
-      admissionYear: '',
+      admissionYear: '',,
+      email: 'wongi8994@gmail.com',
       researchInterests: '',
       avatarUrl: ''
     },
