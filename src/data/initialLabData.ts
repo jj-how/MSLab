2d8ec55eb2095adf6c6280b887a11592dbb423d9
JJ-How <jjh798800@gmail.com>
2026-09-03 17:17:16 +0900

    Update initialLabData.ts

--- src/data/initialLabData.ts
+++ src/data/initialLabData.ts
@@ -600,7 +600,7 @@ We warmly welcome aspiring researchers who are passionate about discovering hidd
     titleEn: 'Lab Introduction',
     descriptionKo: '다변량통계분석연구실 소개',
     descriptionEn: 'Introduction to the Multivariate Statistical Analysis Laboratory',
-    imageUrl: '/sl1.png', // 표지 이미지 1장만 대표로 사용
+    imageUrl: '/s1.jpg', // 표지 이미지 1장만 대표로 사용
     files: [
       { type: 'pdf', url: '/MSL_Introduction.pdf' }
     ]
