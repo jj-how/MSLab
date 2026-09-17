41224be4bbd62c7c5110f5c840cc61514088242b
JJ-How <jjh798800@gmail.com>
2026-09-17 20:26:08 +0900

    Update initialLabData.ts

--- src/data/initialLabData.ts
+++ src/data/initialLabData.ts
@@ -604,15 +604,15 @@ We warmly welcome aspiring researchers who are passionate about discovering hidd
       speaker: '모든 연구원',
       location: '자연과학대학 1호관 - 문헌실',
       files: [
+        {type: 'image',  
+         url: '/Pr20260916.jpg',
+         name: 'Pr20260916'},
         {type: 'pdf',  
          url: '/Pr20260916_Jung.pdf',
          name: 'Pr20260916_Jung'},   
         {type: 'pdf',  
          url: '/Pr20260916_Oh.pdf',
-         name: 'Pr20260916_Oh'},
-        {type: 'image',  
-         url: '/Pr20260916.jpg',
-         name: 'Pr20260916'}
+         name: 'Pr20260916_Oh'}
       ],
       descriptionKo: `      
         A Unified Approach to Interpreting Model Predictions - 정형준
